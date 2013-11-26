29c8f2ee41df0d56ea2db0bf69944f8d6aa7f3d7
T. Budiman <tbudiman@gmail.com>
2013-11-26 16:31:17 +0700

    configuration

--- config/less-css.el
+++ config/less-css.el
@@ -4,4 +4,6 @@
 (add-to-list 'auto-mode-alist '("\\.less$" . less-css-mode))
 (add-to-list 'auto-mode-alist '("\\.css$" . less-css-mode))
 
-;; (setq-default indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq-default css-indent-offset 2)
