f48a36535f26032ee879fee47b3348733543fb2c
T. Budiman <tbudiman@gmail.com>
2013-11-26 15:00:07 +0700

    add web-mode

---
+++ config/web.el
@@ -0,0 +1,15 @@
+(add-lib-path "web-mode")
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.jsp$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+
+(setq-default indent-tabs-mode nil)
+
+(setq web-mode-markup-indent-offset 2)
+
+(setq web-mode-indent-style 2)
+
+(setq web-mode-css-indent-offset 2)
+
+(setq web-mode-code-indent-offset 2)
